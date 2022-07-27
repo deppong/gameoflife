25df24d89da43afb15e0959b30d855bb499c3d86
deppong <30512867+deppong@users.noreply.github.com>
2022-07-26 23:10:34 -0400

    Finished mostly, added drawing with the mouse, still a bug where you can't draw on the borders

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gameoflife
+# John Conway's Game of Life
 Game of Life written in C/SDL2
+
+
+## build
+For windows, copy the lib and include directories to this directory 
+<br>
+
+For Linux you must install `cmake` and `libsdl2-dev`
+run the commands
+
+```
+cmake -B build -S src
+```
+
+On windows you must edit in `/build/CMakeCache.txt` the SDL2_DIR variable to the path of this folder.
+
+afterwards run the following commands on either windows or linux:
+```
+cmake -B build -S src
+cd build
+cmake --build .
+./Debug/gameoflife
+```
--- src/main.c
+++ src/main.c
@@ -3,77 +3,124 @@
     programmed in C/SDL2 by Isaiah Deppong
 
 */
-#ifdef __unix__
-
-    #include <SDL2/SDL.h>
-
-#elif defined(_WIN32)
-
-    #include <SDL.h>
-
-#endif
-
+#include "SDL.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 const int WIDTH  = 800;
 const int HEIGHT = 800;
-const int res    =  40;
+const int res    =  10;
 
 int main(int argc, char**argv) {
     // Initialization -------------------- 
     int quit = 1;
+    int pause = 1;
+    int drawing = 0;
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_Event e;
 
     srand(time(0));
 
     SDL_Window *window = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    const int num_cells = (WIDTH*HEIGHT)/res;
-    const int cell_w = WIDTH/res;
-    const int cell_h = HEIGHT/res;
+    const int cols = WIDTH/res;
+    const int rows = HEIGHT/res;
 
-    uint8_t *grid = malloc(sizeof(uint8_t) * num_cells);
-    for (int i = 0; i < num_cells; i++) {
-        grid[i] = rand() % 2;
+    uint8_t *grid = malloc(sizeof(uint8_t) * cols*rows);
+    uint8_t *next_grid = malloc(sizeof(uint8_t) * cols*rows);
+    for (int i = 0; i < cols*rows; i++) {
+        grid[i] = 0;
+        next_grid[i] = 0;
     }
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    SDL_Event e;
 
     while(quit) {
         SDL_PollEvent(&e);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
 
         switch (e.type) {
             case SDL_QUIT: quit=0; break;
+            case SDL_MOUSEBUTTONDOWN: drawing = 1; break;
+            case SDL_MOUSEBUTTONUP:   drawing = 0; break;
+            case SDL_MOUSEMOTION:
+                if (drawing) {
+                    int x = e.button.x;
+                    int y = e.button.y;
+                    int cell_x = (x-res+1)/res+1;
+                    int cell_y = (y-res+1)/res+1;
+                    grid[cell_x + cell_y*cols] = 1;
+                }
+                break;
+            case SDL_KEYDOWN:
+                switch (e.key.keysym.sym) {
+                    case SDLK_SPACE:
+                        pause = pause?0:1;
+                        break;
+                    default:
+                        break;
+                }break;
         }
 
         // display each cell
-        for (int i = 0; i < res; i++) {
-            for (int j = 0; j < res; j++) {
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
                 SDL_Rect rect = {
-                    cell_w + i*cell_w, 
-                    cell_h + j*cell_h, 
-                    .w=cell_w, 
-                    .h=cell_h
+                    i*res, 
+                    j*res, 
+                    .w=res-1, 
+                    .h=res-1,
                 };
-                if (grid[i + j*WIDTH]) {
-                    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
-                } else {
+
+                if (grid[i + j*cols]) {
                     SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255);
+                } else {
+                    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
                 }
                 SDL_RenderFillRect(renderer, &rect);
             }
         }
 
+        // update each cell
+        if (!pause) { 
+            for (int x = 0; x < cols; x++) {
+                for (int y = 0; y < rows; y++) {
+
+                    int neighbors = 0;
+                    for (int i = -1; i < 2; i++) {
+                        for (int j = -1; j < 2; j++) {
+                            neighbors += grid[((x+i+cols)%cols) + ((y+j+rows)%rows)*cols];
+                        }
+                    }
+                    neighbors -= grid[x+y*cols];
+
+                    if (grid[x+y*cols]) {
+                        next_grid[x+y*cols] = (neighbors == 2 || neighbors == 3);
+                    } else {
+                        next_grid[x+y*cols] = neighbors == 3;
+                    }
+                } 
+            } 
+            
+            for (int i = 0; i < cols*rows; i++) {
+                grid[i] = next_grid[i];
+            }
+
+            SDL_Delay(25);
+        }
+
+
+
         SDL_RenderPresent(renderer);
     }
 
     free(grid);
+    free(next_grid);
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     return 0;
